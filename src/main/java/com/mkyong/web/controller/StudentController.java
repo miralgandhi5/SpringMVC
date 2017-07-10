fa15041629898739866533f6c02feebc1b3e8295
miralgandhi <miralgandhi5@gmail.com>
2017-07-10 14:41:37 +0530

    add

---
+++ src/main/java/com/mkyong/web/controller/StudentController.java
@@ -0,0 +1,37 @@
+package com.mkyong.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class StudentController extends MultiActionController {
+
+
+	protected ModelAndView dummy(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws Exception {
+		ModelAndView modelAndView=new ModelAndView("index");
+		return modelAndView;
+
+	}
+
+	public void dummy2(HttpServletRequest httpServletRequest,
+					   HttpServletResponse httpServletResponse) throws Exception {
+		httpServletResponse.setContentType("text/html");
+		httpServletResponse.getWriter().println("<b>HELLO World</b>");
+	}
+
+
+
+
+
+
+
+
+
+}
---
+++ src/main/webapp/WEB-INF/views/jsp/index.jsp
@@ -0,0 +1,15 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: miral
+  Date: 7/10/2017
+  Time: 1:53 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+Hello from index.gsp
+</body>
