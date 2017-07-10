8186287a2eaa4889d8765712c852539c9ad56669
miralgandhi <miralgandhi5@gmail.com>
2017-07-10 15:03:36 +0530

    add

--- src/main/java/com/mkyong/web/controller/StudentController.java
+++ src/main/java/com/mkyong/web/controller/StudentController.java
@@ -1,37 +1,20 @@
 package com.mkyong.web.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
-import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 
-public class StudentController extends MultiActionController {
+@Controller
+public class StudentController {
 
 
-	public ModelAndView dummy(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws Exception {
-		ModelAndView modelAndView=new ModelAndView("index");
-		return modelAndView;
-
-	}
-
-	public void dummy2(HttpServletRequest httpServletRequest,
-					   HttpServletResponse httpServletResponse) throws Exception {
-		httpServletResponse.setContentType("text/html");
-		httpServletResponse.getWriter().println("<b>HELLO World</b>");
-	}
-
-
-
-
-
-
-
+    @RequestMapping("/")
+    @ResponseBody
+    ModelAndView index() {
+        return new ModelAndView("index string");
+    }
 
 
 }
