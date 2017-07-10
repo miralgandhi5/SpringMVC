f0c3dce3619d9ab036167e10cabfb3cd7801e629
miralgandhi <miralgandhi5@gmail.com>
2017-07-10 15:57:16 +0530

    add

--- src/main/java/com/mkyong/web/controller/StudentController.java
+++ src/main/java/com/mkyong/web/controller/StudentController.java
@@ -1,10 +1,10 @@
 package com.mkyong.web.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.Map;
 
 
 @Controller
@@ -14,11 +14,8 @@ public class StudentController {
 
     @RequestMapping(value = "/user", method = RequestMethod.POST)
     @ResponseBody
-    ModelAndView pathDemo(@RequestParam("firstName") String firstName, @RequestParam("lastName") String lastName) {
-        ModelAndView modelAndView = new ModelAndView("index");
-        modelAndView.addObject("firstName", firstName);
-        modelAndView.addObject("lastName", lastName);
-        return modelAndView;
+    String submitForm(User user) {
+        return "FirstName " + user.getFirstName() + " LastName " + user.getLastName();
     }
 
 
--- src/main/java/com/mkyong/web/controller/User.java
+++ src/main/java/com/mkyong/web/controller/User.java
@@ -4,20 +4,26 @@ package com.mkyong.web.controller;
  * Created by miral on 7/6/2017.
  */
 public class User {
-    String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public User(String name) {
-        this.name = name;
-    }
+    String firstName;
+    String lastName;
 
     public User() {
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
+
